40f62715380771a9442f449bbdd8f891a837c4f2
naod24-coder <naod5180@gmail.com>
2025-06-23 10:43:17 -0400

    switch to selenium

--- code/project.py
+++ code/project.py
@@ -1,27 +1,35 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import json
+import time
 
-def fetch_entry(tid):
-  base_url = "https://trm.oit.va.gov/ToolPage.aspx"
-  param = {"tid": tid, "tab": 2}
-  headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"}
-  resp = requests.get(base_url, params=param, headers=headers)
-  resp.raise_for_status()
-  return resp.text, resp.url
+def fetch_data(tid):  
+  url = f"https://www.oit.va.gov/Services/TRM/ToolPage.aspx?tid={tid}&tab2"
+  
+  options = webdriver.ChromeOptions()
+  options.add_argument("--headless ")
+  driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-def parse_trm(html, url):
-  soup = BeautifulSoup(html, "html.parser")
-  entry = {}
-  entry["name"] = soup.select_one("#ctl00$ContentPlaceHolder1$hdnToolName").get_text(strip =True)
-  entry["tid"] = int(soup.select_one("#ctl00$ContentPlaceHolder1$hdnToolID").get_text(strip =True)) # Added .get_text()
-  entry["decision"] = soup.select_one("#ctl00_ContentPlaceHolder1_lblDecision").get_text(strip =True)
-  entry["verison"] = soup.select_one("#ctl00_ContentPlaceHolder1_lblVersion").get_text(strip =True)
-  entry["approve_date"] = soup.select_one("#ctl00_ContentPlaceHolder1_lblApproveDate").get_text(strip =True)
-  return {"trm_base_url": url.split("?")[0], "trm_entries":[entry]}
+  try:
+     driver.get(url)
+     time.sleep(3)
+     entry = {
+          "name": driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_hdnToolName").text, 
+          "tid": driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_hdnToolID").text, 
+          "decision": driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_hdnDecision").text,
+          "version": driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_hdnVersion").text, 
+          "approval_date": driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_hdnDecisionDate").text
+          }
+     return {
+     "trm_base_url": url.split("?")[0],
+     "trm_entries":[entry]
+     }
+  finally:
+     driver.quit()
 
 if __name__ == "__main__":
     tid = 6367
-    html, url = fetch_entry(tid)
-    entry = parse_trm(html, url)
-    print(entry)
+    result = fetch_data(tid)
+    print(json.dumps(result, indent=2))
