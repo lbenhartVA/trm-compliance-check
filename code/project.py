2edf86d1a8a48e19b3b442388365ff32286ecd02
naod24-coder <naod5180@gmail.com>
2025-06-24 09:07:50 -0400

    webdriver works now

--- code/project.py
+++ code/project.py
@@ -1,23 +1,25 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.edge.service import Service 
+from selenium.webdriver.edge.options import Options
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 import json
 import time
+import os
 
 def fetch_data(tid):  
   url = f"https://www.oit.va.gov/Services/TRM/ToolPage.aspx?tid={tid}&tab2"
-  
-  options = webdriver.ChromeOptions()
-  options.add_argument("--headless ")
-  driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+  path = "C:\\Users\\abrahamn\\.vscode\\trm-compliance-check\\msedgedriver.exe"
+  options = Options()
+
 
   try:
+     driver = webdriver.Edge(service=Service(path), options=options)
      driver.get(url)
      time.sleep(3)
+
      entry = {
-          "name": driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_hdnToolName").text, 
-          "tid": driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_hdnToolID").text, 
+          "name": driver.find_element(By.ID, "ContentPlaceHolder1_hdnToolName").text, 
+          "tid": int(driver.find_element(By.ID, "ContentPlaceHolder1_hdnToolID").text), 
           "decision": driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_hdnDecision").text,
           "version": driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_hdnVersion").text, 
           "approval_date": driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_hdnDecisionDate").text
@@ -30,6 +32,5 @@ def fetch_data(tid):
      driver.quit()
 
 if __name__ == "__main__":
-    tid = 6367
-    result = fetch_data(tid)
+    result = fetch_data(6367)
     print(json.dumps(result, indent=2))
