02e72159b200600107410ee3e3619956bb1adcd2
naod24-coder <naod5180@gmail.com>
2025-06-30 11:48:26 -0400

    new changes

--- code/project.py
+++ code/project.py
@@ -1,63 +1,56 @@
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service 
-from selenium.webdriver.edge.options import Options
 from selenium.webdriver.common.by import By
 import json
 import time
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import re
+from datetime import datetime
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import WebDriverException, SessionNotCreatedException, TimeoutException, StaleElementReferenceException, NoSuchElementException
+
+def get_current_quarter():
+   now = datetime.now()
+   current_year = now.year
+   current_quarter = f"Q{(now.month - 1) // 3 + 1}"
+   current_year, current_quarter
 
 def fetch_data(tid):  
   url = f"https://www.oit.va.gov/Services/TRM/ToolPage.aspx?tid={tid}&tab2"
-  path = "C:\\Users\\abrahamn\\.vscode\\trm-compliance-check\\msedgedriver.exe"
-  options = Options()
-
+  
 
   try:
-     #Sets up the edgedriver
-     driver = webdriver.Edge(service=Service(path), options=options)
+     driver = webdriver.Chrome(service= Service(ChromeDriverManager().install()))
      driver.get(url)
      time.sleep(30)
-
+  except (ConnectionError, WebDriverException, TimeoutException, SessionNotCreatedException) as e:
+     print("failed to download edgedriver using webdriver-manager:")
+     print(e)
+     return
+  try:
+     
      #
-     label = driver.find_element(By.XPATH, "//td[text()='Decision Date:']")
-     value = label.find_element(By.XPATH, "following-sibling::td")
-     f_table = driver.find_element(By.XPATH, "//table[.//a[text()='Past']]]")
-     found = False
+     decision_date_path = driver.find_element(By.XPATH, "//td[text()='Decision Date:']")
+     decision_date = decision_date_path.find_element(By.XPATH, "following-sibling::td")
+     
+     table = driver.find_element(By.CSS_SELECTOR, "table:nth-of-type(4)")
+     
      decision = None
-     a_decision = None
-     if f_table:
-        rows = f_table.find_elements(By.TAG_NAME, "tr")
-        for row in rows:
-           cells = row.find_elements(By.TAG_NAME ,"td")
-           if not cells:
-              continue
-           version = cells[0].text.strip()
-           for cell in cells:
-              style = cell.get_attribute("style") or ""
-              if "border-color: black" in style and "border-width: 2px" in style:
-                 decision = cell.text.strip()
-                 if "Authorized" in decision:
-                    a_verison = version
-                    a_decision = decision
-                    found = True   
-                    break
-           if found:
-              break
-
+     verison = None
+     #decision = driver.find_element(By.CSS_SELECTOR, "table:nth-of-type(4) tbody tr:nth-of-type(2) td:nth-of-type(5)").text
+     #verison = driver.find_element(By.CSS_SELECTOR, "table:nth-of-type(4) thead tr:nth-of-type(5) th:nth-of-type(7)").text
      entry = {
           "URL": url,
           "Name": driver.title.strip(), 
           "Tid": driver.find_element(By.ID, "ContentPlaceHolder1_hdnToolId").get_attribute("value"),
-          "Version": a_verison,
-          "Decision": a_decision,
-          "Decision_Date": value.text.split(" ")[0]
+          "Version": verison,
+          "Decision": decision,
+          "Decision_Date": decision_date.text.split(" ")[0]
           }
      return {
      "trm_base_url": url.split("?")[0],
      "trm_entries":[entry]
      }
+  except(NoSuchElementException, TimeoutException, StaleElementReferenceException):
+     print("Error locating or interactinng with the page elements")
   finally:
      driver.quit()
 
